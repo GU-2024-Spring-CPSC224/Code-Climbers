4296863ebd2b795004ea8f13f847b6d66ddced94
Steven Deibert <stevekdeibert@gmail.com>
2024-03-27 11:40:40 -0700

    Create Player.java

---
+++ src/main/java/chutesandladders/Player.java
@@ -0,0 +1,22 @@
+package chutesandladders;
+
+//    Purpose: Represent a player in the game.
+//    Define player attributes like name, current position, etc.
+//    Implement methods to move the player on the board.
+
+public class Player {
+    private String name;
+    private int currentPosition;
+
+    public Player(String name) {
+        this.name = name;
+        this.currentPosition = 0;
+    }
+
+    public void move(int steps) {
+        // Update currentPosition based on steps
+    }
+
+    // Getter and Setter methods
+}
+
