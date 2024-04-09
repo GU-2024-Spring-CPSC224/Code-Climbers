d50ce9a1c845093256df66968c52f10e29278ddf
Will G <wg00014@mix.wvu.edu>
2024-04-09 09:41:53 -0700

    added array initialization boardArray[] to gameboard class

--- src/main/java/chutesandladders/GameBoard.java
+++ src/main/java/chutesandladders/GameBoard.java
@@ -16,6 +16,8 @@ import java.util.Random;
 
 public class GameBoard extends JPanel {
 	// Define board dimensions, chutes, ladders, etc.
+    Integer[] boardArray = new Integer[100];
+    ///array of 100 zeros
 	
 	// Hashmap for ladders, key, value int pair of start, end
 	HashMap<Integer, Integer> ladders;
