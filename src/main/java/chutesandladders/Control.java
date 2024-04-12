1f75b0568285323ee81537652cccebcd17251812
Steven Deibert <stevekdeibert@gmail.com>
2024-04-12 16:23:25 -0700

    JFrame Minimum Size

--- src/main/java/chutesandladders/Control.java
+++ src/main/java/chutesandladders/Control.java
@@ -10,6 +10,7 @@
 package chutesandladders;
 
 import javax.swing.JFrame;
+import java.awt.*;
 import java.util.List;
 
 public class Control {
@@ -55,6 +56,7 @@ public class Control {
     private void createUI(GameBoard board) {
         UI = new Interface(board, this);
 	    JFrame frame = new JFrame("Chutes and Ladders");
+        frame.setMinimumSize(new Dimension(800, 600));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(UI);
         frame.pack();
