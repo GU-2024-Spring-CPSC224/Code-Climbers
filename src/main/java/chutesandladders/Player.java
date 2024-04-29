45d9e4e714a8b254319be31848474084442fb9c2
Steven Deibert <stevekdeibert@gmail.com>
2024-04-29 15:26:58 -0700

    Update Player.java

--- src/main/java/chutesandladders/Player.java
+++ src/main/java/chutesandladders/Player.java
@@ -3,7 +3,7 @@
  * CPSC 224, Spring 2024
  * Final Project
  * Sources: N/A
- * 
+ *
  * @author William Garlington, Steve Deibert, Manny Uzoma
  * @version 1.0
  */
@@ -12,88 +12,88 @@ package chutesandladders;
 import java.awt.*;
 
 public class Player {
-    private String name;
+	private String name;
 	public int currentPosition;
-    private static int playercount;
-    private final Integer playerID;
-    private Color myColor;
+	private static int playercount;
+	private final Integer playerID;
+	private Color myColor;
 	
 	/**
-     * Initializes a new player with the given name.
-     *
-     * @param name The name of the player.
-     */
-    public Player(String name) {
-        playercount += 1;
-        if (name.isEmpty()) {
-            name = "Player" + playercount;
-        }
-        this.name = name;
-        this.currentPosition = 1;
-	    playerID = playercount;
-        assignColor();
-    }
-    
-    // Getter and Setter methods
-
-    //get name
-    public String getPlayerName() {
-        return name;
-    }
-
-    public void setPlayerName(String input) {
-        name = input;
-    }
-
-    //get ID
-    public Integer getPlayerID() {
-        return playerID;
-    }
-
-    public Color getColor() {
-        return myColor;
-    }
-
-    private void assignColor() {
-        switch (playerID) {
-            case 1:
-                myColor = Color.GREEN;
-                break;
-            case 2:
-                myColor = Color.ORANGE;
-                break;
-            case 3:
-                myColor = Color.MAGENTA;
-                break;
-            case 4:
-                myColor = Color.BLUE;
-                break;
-            default:
-                myColor = Color.LIGHT_GRAY;
-                break;
-        }
-    }
-
-    
-    /**
-     * Gets the current position of the player on the board.
-     *
-     * @return The current position of the player.
-     */
-    public int getCurrentPosition() {
-        return currentPosition;
-    }
-    
-    /**
-     * Sets the current position of the player on the board.
-     *
-     * @param inputPosition The new position to set.
-     */
-    public void setCurrentPosition(int inputPosition) {
-        if (inputPosition >= 100) {
-            currentPosition = 100;
-            return;
-        }
-        currentPosition = inputPosition;
-    }
+	 * Initializes a new player with the given name.
+	 *
+	 * @param name The name of the player.
+	 */
+	public Player(String name) {
+		playercount += 1;
+		if (name.isEmpty()) {
+			name = "Player" + playercount;
+		}
+		this.name = name;
+		this.currentPosition = 1;
+		playerID = playercount;
+		assignColor();
+	}
+	
+	// Getter and Setter methods
+	
+	//get name
+	public String getPlayerName() {
+		return name;
+	}
+	
+	public void setPlayerName(String input) {
+		name = input;
+	}
+	
+	//get ID
+	public Integer getPlayerID() {
+		return playerID;
+	}
+	
+	public Color getColor() {
+		return myColor;
+	}
+	
+	private void assignColor() {
+		switch (playerID) {
+			case 1:
+				myColor = Color.GREEN;
+				break;
+			case 2:
+				myColor = Color.ORANGE;
+				break;
+			case 3:
+				myColor = Color.MAGENTA;
+				break;
+			case 4:
+				myColor = Color.BLUE;
+				break;
+			default:
+				myColor = Color.LIGHT_GRAY;
+				break;
+		}
+	}
+	
+	
+	/**
+	 * Gets the current position of the player on the board.
+	 *
+	 * @return The current position of the player.
+	 */
+	public int getCurrentPosition() {
+		return currentPosition;
+	}
+	
+	/**
+	 * Sets the current position of the player on the board.
+	 *
+	 * @param inputPosition The new position to set.
+	 */
+	public void setCurrentPosition(int inputPosition) {
+		if (inputPosition >= 100) {
+			currentPosition = 100;
+			return;
+		}
+		currentPosition = inputPosition;
+	}
 }
