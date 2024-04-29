55b2c1710ba5696e4897350ec5f95afd53afee3d
Steven Deibert <stevekdeibert@gmail.com>
2024-04-29 15:27:05 -0700

    Update Die.java

--- src/main/java/chutesandladders/Die.java
+++ src/main/java/chutesandladders/Die.java
@@ -4,33 +4,33 @@ package chutesandladders;
 import java.util.Random;
 
 public class Die implements Comparable<Die> {
-    private Integer sideUp; // Current die 'value' in range 1..numSides
-    private final Integer numSides; // Sides on the die (should be 1...INF integer)
+	private Integer sideUp; // Current die 'value' in range 1..numSides
+	private final Integer numSides; // Sides on the die (should be 1...INF integer)
 	
 	public Die(Integer numSides, Integer startingSide) {
-        this.numSides = numSides;
-        this.sideUp = startingSide;
-    }
-    
-    public void roll() {
-        Random rand = new Random();
-        this.sideUp = rand.nextInt(this.numSides) + 1;
-    }
-    
-    public Integer getSideUp() {
-        return this.sideUp;
-    }
-    
-    @Override
-    public String toString() {
-        String ret = "";
-        // ret += "Die: " + this.sideUp.toString() + " of " + this.numSides.toString() + " sides";
-        ret += this.sideUp.toString();
-        return ret;
-    }
-    
-    @Override
-    public int compareTo(Die otherDie) {
-        return this.sideUp.compareTo(otherDie.sideUp);
-    }
+		this.numSides = numSides;
+		this.sideUp = startingSide;
+	}
+	
+	public void roll() {
+		Random rand = new Random();
+		this.sideUp = rand.nextInt(this.numSides) + 1;
+	}
+	
+	public Integer getSideUp() {
+		return this.sideUp;
+	}
+	
+	@Override
+	public String toString() {
+		String ret = "";
+		// ret += "Die: " + this.sideUp.toString() + " of " + this.numSides.toString() + " sides";
+		ret += this.sideUp.toString();
+		return ret;
+	}
+	
+	@Override
+	public int compareTo(Die otherDie) {
+		return this.sideUp.compareTo(otherDie.sideUp);
+	}
 }
