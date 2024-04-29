6a718de7fe82d2e90612fbe18829e49a7e89dddd
Steven Deibert <stevekdeibert@gmail.com>
2024-04-29 15:07:49 -0700

    Update ChutesAndLadders.java

--- src/main/java/chutesandladders/ChutesAndLadders.java
+++ src/main/java/chutesandladders/ChutesAndLadders.java
@@ -3,7 +3,7 @@
  * CPSC 224, Spring 2024
  * Final Project
  * Sources: N/A
- * 
+ *
  * @author William Garlington, Steve Deibert, Manny Uzoma
  * @version 1.0
  */
@@ -17,9 +17,9 @@ import javax.swing.*;
 
 public class ChutesAndLadders {
 	public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            PlayerSelectionGUI playerSelectionGUI = new PlayerSelectionGUI();
-            playerSelectionGUI.setVisible(true);
-        });
-    }
+		SwingUtilities.invokeLater(() -> {
+			PlayerSelectionGUI playerSelectionGUI = new PlayerSelectionGUI();
+			playerSelectionGUI.setVisible(true);
+		});
+	}
 }
