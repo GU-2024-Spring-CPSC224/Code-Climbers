a322d77962204410e16f3cba929391e37c652ad6
W-Garlington <wsgarlington@gmail.com>
2024-04-30 22:10:52 -0700

    made scoreboard look a lot better and display player colors

--- src/main/java/chutesandladders/Interface.java
+++ src/main/java/chutesandladders/Interface.java
@@ -230,13 +230,14 @@ public class Interface extends JPanel {
 				g.setColor(Color.BLACK);
 			}
 			g.setColor(player.getColor());
-			g.fillRect(xcen + 5, y + 15, 100, 35);
+			g.fillRect(xcen, y, 15, 60);
 			g.setColor(Color.BLACK);
+			g.fillRect(xcen + 10, y, 5, 60);
 			char[] nameChars = playerList.get(i).getPlayerName().toCharArray();
 			char[] posChars = String.valueOf(playerList.get(i).getCurrentPosition()).toCharArray();
 			g.drawRect(xcen, y, 200, 60);
-			g.drawChars(nameChars, 0, nameChars.length, xcen + 15, y + 40);
-			g.drawChars(posChars, 0, posChars.length, xcen + 150, y + 40);
+			g.drawChars(nameChars, 0, nameChars.length, xcen + 25, y + 40);
+			g.drawChars(posChars, 0, posChars.length, xcen + 170, y + 40);
 			i++;
 		}
 	}
